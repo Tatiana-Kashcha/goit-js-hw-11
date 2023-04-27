f3cdd65c84e59727e93990e185af93400762870d
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-27 11:29:58 +0300

    додає notiflix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 // API key: 35802971-9f205e77cee7d2465290329c6
