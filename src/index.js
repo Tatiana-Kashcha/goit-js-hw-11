6900375645b0f397d765b9f5e437cdf7f070e06f
Tatiana Kashcha <tkashcha@gmail.com>
2023-04-28 12:45:40 +0300

    add thumb-img

--- src/index.js
+++ src/index.js
@@ -60,22 +60,24 @@ function createMarkup(arr) {
     .map(
       ({ webformatURL, tags, likes, views, comments, downloads }) =>
         `<div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" width="100%"/>
-        <div class="info">
-            <p class="info-item">
-            <b>Likes</b>${likes}
-            </p>
-            <p class="info-item">
-            <b>Views</b>${views}
-            </p>
-            <p class="info-item">
-            <b>Comments</b>${comments}
-            </p>
-            <p class="info-item">
-            <b>Downloads</b>${downloads}
-            </p>
-        </div>
-     </div>`
+            <div class="thumb-img">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+            </div>
+            <div class="info">
+                <p class="info-item">
+                <b>Likes</b>${likes}
+                </p>
+                <p class="info-item">
+                <b>Views</b>${views}
+                </p>
+                <p class="info-item">
+                <b>Comments</b>${comments}
+                </p>
+                <p class="info-item">
+                <b>Downloads</b>${downloads}
+                </p>
+            </div>
+        </div>`
     )
     .join('');
 }
